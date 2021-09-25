1c73f30233de68e2f50e49f18c2aa40acf9d082c
maliayyildiz <maliayyiliz61@gmail.com>
2021-09-25 15:55:48 +0200

    codes

--- src/test/java/stepdefinitions/CustomMethodStepDefs.java
+++ src/test/java/stepdefinitions/CustomMethodStepDefs.java
@@ -5,7 +5,7 @@ import io.cucumber.java.en.When;
 public class CustomMethodStepDefs {
     @When("user click on {string}")
     public void user_click_on(String pageName) {
-        ReusableMethods.scrollIntoViewWithUiScrollable(pageName);
+        //ReusableMethods.scrollIntoViewWithUiScrollable(pageName);
     }
 
 }
